e688779bf4df0fff3fe55868d215a06f4bec6e57
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-11-11 21:38:47 +0200

    express-fundamentals

---
+++ Working with ExpressJS/installing-express/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+
+    console.log('In the middleware!');
+});
+
+const server = http.createServer(app);
+server.listen(3000);
